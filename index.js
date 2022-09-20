e58e2ccf317425a5ec6123fc01da7d45d81b9e6a
tushar199627 <tusharbarai6@gmail.com>
2022-09-20 17:04:30 +0530

    updated

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict';
+// console.log('Hi');
+
+const getData = async function () {
+    const response = await fetch(`https://backendtusharnew.herokuapp.com/getuser`);
+    const response_data = await response.json();
+    // console.log(response_data.data);
+    return response_data.data;
+};
+
+const login_data = async function () {
+    const login_data = await getData();
+    console.log(login_data);
+
+    const body = document.querySelector('table');
+    
+    login_data.forEach(function(data){
+        console.log(data);
+        const html = `<tr><td>${data.name}</td><td>${data.emailId}</td></tr>`;
+        body.insertAdjacentHTML('beforeend', html);
+    });
+
+};
+
+login_data();
+// console.log(login_data);
